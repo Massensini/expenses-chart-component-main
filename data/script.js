4364884afda115ff728e5734d950112bdaba8b76
Gabriel Massensini <89725700+Massensini@users.noreply.github.com>
2022-10-12 11:25:44 -0300

    js interactive finished

--- data/script.js
+++ data/script.js
@@ -1,3 +1,17 @@
-const { day, amount } = require('./data.json')
+const values = Array.from(document.querySelectorAll('.graph-column'))
+const DayValue = Array.from(document.querySelectorAll('.value-day'))
 
-console.log(`${day}, ${amount}`)
+values.forEach(e => {
+    let i = values.indexOf(e)
+
+    e.addEventListener('mouseover', () => {
+        e.classList.add('active')
+        DayValue[i].classList.add('value-day-active')        
+    })
+
+    e.addEventListener('mouseout', () => {
+        e.classList.remove('active')
+        DayValue[i].classList.remove('value-day-active')   
+    })
+})
+
